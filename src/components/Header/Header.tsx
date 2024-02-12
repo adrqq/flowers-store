c66882c0027e64ccf10cf70c6e27840de7b0cadd
Sasha-Maniak <116157371+metalikaqq@users.noreply.github.com>
2024-02-12 16:53:48 +0200

    button
    add new button

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,12 @@
+import FlowerButton from "@/UI/FlowerButton/FlowerButton";
+
 export type HeaderProps = {
   // props go here
 };
 export default function Header(props: HeaderProps) {
   return (
     <header>
-      <h1>Header</h1>
+      <FlowerButton text="Заказать" />
     </header>
   );
 }
